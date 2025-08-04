0117b142d9138911b1fcb8ec2187ad6cd4f94c9f
SynixY <bentenroben35@gmail.com>
2025-08-04 23:22:10 +0200

    asdas

--- src/app/components/home-page/footer.tsx
+++ src/app/components/home-page/footer.tsx
@@ -13,13 +13,20 @@ export default function Footer() {
             alignContent: "center",
           }}
         >
-          <a className="jsx-ad15c1348c48af6b" href="https://bonk.fun/">
+          <a
+            className="jsx-ad15c1348c48af6b"
+            href="https://letsbonk.fun/token/"
+          >
             CA: Coming soon
           </a>
           <hr className="jsx-ad15c1348c48af6b" />
 
           <section className="jsx-ad15c1348c48af6b">
-            <a href="https://x.com/hangarcade" target="_blank" rel="noreferrer">
+            <a
+              href="https://x.com/BonkArcade_Fun"
+              target="_blank"
+              rel="noreferrer"
+            >
               {" "}
               <BsTwitterX style={{ color: "white" }} />
             </a>
--- src/app/stores/useGameStore.ts
+++ src/app/stores/useGameStore.ts
@@ -4,7 +4,7 @@ import { persist, PersistOptions } from "zustand/middleware";
 import { io, Socket } from "socket.io-client";
 import { jwtDecode } from "jwt-decode";
 
-const BACKEND_URL = "https://9yfbbh3enhk8ll-8000.proxy.runpod.net"; //" "http://localhost:8000"
+const BACKEND_URL = "https://9yfbbh3enhk8ll-8000.proxy.runpod.net"; //" ""
 
 type Player = {
   username: string;
