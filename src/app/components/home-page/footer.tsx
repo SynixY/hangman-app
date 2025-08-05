631b3df95407838f15d9de220041a4776e4ff27e
SynixY <bentenroben35@gmail.com>
2025-08-05 21:06:13 +0200

    asd

--- src/app/components/home-page/footer.tsx
+++ src/app/components/home-page/footer.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+import { BACKEND_URL } from "@/app/stores/useGameStore";
+import React, { useEffect, useState } from "react";
 import { BsTwitterX } from "react-icons/bs";
 
 export default function Footer() {
+  const [treasuryAddress, setTreasuryAddress] = useState("Loading...");
+
+  useEffect(() => {
+    // Fetch the address from the new backend endpoint
+    const fetchAddress = async () => {
+      try {
+        const response = await fetch(`${BACKEND_URL}/api/config/treasury`);
+        if (!response.ok) {
+          console.error("Failed to fetch address");
+        }
+        const data = await response.json();
+        if (data.reward_token_mint_address) {
+          setTreasuryAddress(data.reward_token_mint_address);
+        } else {
+          setTreasuryAddress("not live yet");
+        }
+      } catch (error) {
+        console.error("Could not fetch treasury address:", error);
+        setTreasuryAddress("not live yet");
+      }
+    };
+
+    fetchAddress();
+  }, []);
   return (
     <footer className="jsx-ad15c1348c48af6b">
       <span className="jsx-ad15c1348c48af6b" style={{ width: "100%" }}>
@@ -17,7 +43,7 @@ export default function Footer() {
             className="jsx-ad15c1348c48af6b"
             href="https://letsbonk.fun/token/"
           >
-            CA: Coming soon
+            CA: {treasuryAddress}
           </a>
           <hr className="jsx-ad15c1348c48af6b" />
 
